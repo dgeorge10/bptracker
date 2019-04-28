267818469dffc5eb8c3157cc0fe5d977b1b79490
Dennis George <djg365@drexel.edu>
2019-04-28 00:55:42 -0400

    adding pushing

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+window.onload= function(){
+	getData();
+}
 var config = {
 	apiKey: "AIzaSyB6FGuMtHqxuG-VfQecIFlXoH0KGKXWhN4",
 	authDomain: "bpstracker.firebaseapp.com",
@@ -12,23 +15,29 @@ let values = []
 let sugar = []
 let bp = []
 let dates;
-var database = firebase.database().ref("User");
-database.once('value', hasData, noData);
-function hasData(data){
-	user = data.val();
-	dates = Object.keys(user);
-	for(let i=0; i<dates.length; i++){
-		values.push(user[dates[i]]);
+function getData(){
+	values = [];
+	sugar = [];
+	bp = [];
+	var database = firebase.database().ref("User");
+	database.once('value', hasData, noData);
+	function hasData(data){
+		user = data.val();
+		dates = Object.keys(user);
+		for(let i=0; i<dates.length; i++){
+			values.push(user[dates[i]]);
+		}
+		for(let i=0; i<values.length; i++){
+			sugar.push(values[i].sugar);
+			bp.push(values[i].bp);
+		}
+		drawData()
 	}
-	for(let i=0; i<values.length; i++){
-		sugar.push(values[i].sugar);
-		bp.push(values[i].bp);
+	function noData (err) {
+		console.log(err);
 	}
-	drawData();
 }
-function noData (err) {
-	console.log(err);
-}
+
 function drawData(){
 	var ctx = document.getElementById('myChart').getContext('2d');
 	var myChart = new Chart(ctx, {
@@ -41,15 +50,15 @@ function drawData(){
 				borderColor: 'rgba(255,0,0,1)',
 				fill: false
 			},
-			{
-				label: 'Blood Sugar',
-				data: sugar,
-				borderColor: 'rgba(0,255,0,1)',
-				fill: false
-			}
-		]},
+				{
+					label: 'Blood Sugar',
+					data: sugar,
+					borderColor: 'rgba(0,255,0,1)',
+					fill: false
+				}
+			]},
 		options: {
-			responsive: false,
+			responsive: true,
 			scales: {
 				yAxes: [{
 					ticks: {
@@ -59,7 +68,21 @@ function drawData(){
 			}
 		}
 	});
+}
+function sendData(){
+	let bp = document.getElementById("bp").value;
+	let sugar = document.getElementById("sugar").value;
+	let today = new Date();
+	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	if(bp && sugar){
+		firebase.database().ref('/User/' + date).set({
+			bp: parseInt(bp),
+			sugar: parseInt(sugar)
+		}, function(err){
+			if(err){console.log(err);}
+			else{getData();}
+		});
 
+	}
 
 }
-
